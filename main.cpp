332904b3953bacbac821e872477b8c998ec1dcd9
Матвиенко Федор <f.matvienko@infsys.ru>
2023-05-29 14:46:20 +0300

    Добавлен OpenGl для Windows

--- main.cpp
+++ main.cpp
@@ -12,63 +12,63 @@ int main()
     string s1 = config["first"].asString();
     string s2 = config["second"].asString();
     cout << s1 << " " << s2 << endl;
-    while(true);
-    // // Инициализация GLFW
-    // if (!glfwInit()) {
-    //     // Обработка ошибки инициализации GLFW
-    //     return -1;
-    // }
     
-    // // // Создание окна
-    // GLFWwindow* window = glfwCreateWindow(800, 600, "Мое окно", nullptr, nullptr);
-    // if (!window)
-    // {
-    //     // Обработка ошибки создания окна
-    //     glfwTerminate();
-    //     return -1;
-    // }
-    // glfwMakeContextCurrent(window);
-    // // Инициализация ImGui
-    // IMGUI_CHECKVERSION();
-    // ImGui::CreateContext();
+    // Инициализация GLFW
+    if (!glfwInit()) {
+        // Обработка ошибки инициализации GLFW
+        return -1;
+    }
+    
+    // // Создание окна
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Мое окно", nullptr, nullptr);
+    if (!window)
+    {
+        // Обработка ошибки создания окна
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    // Инициализация ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
 
-    // // Настройка ImGui для работы с GLFW
-    // ImGui_ImplGlfw_InitForOpenGL(window, true);
-    // ImGui_ImplOpenGL3_Init("#version 330");
-
-
-
-    // // Основной цикл программы
-    // while (!glfwWindowShouldClose(window)) {
-    //     glClear(GL_COLOR_BUFFER_BIT);
-    //     // Обработка событий
-    //     glfwPollEvents();
-
-    //     // Начало нового кадра ImGui
-    //     ImGui_ImplOpenGL3_NewFrame();
-    //     ImGui_ImplGlfw_NewFrame();    
-    //     ImGui::NewFrame();
-
-    //     // Создание пользовательского интерфейса ImGui
-    //     ImGui::Begin("Hello, ImGui!");
-    //     ImGui::Text("Welcome to ImGui!");
-    //     if (ImGui::Button("Click Me!"))
-    //     {
-    //     }
+    // Настройка ImGui для работы с GLFW
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
 
 
 
-    //     ImGui::End();
-    //             // Рендеринг ImGui
-    //     ImGui::Render();
-    //     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    // Основной цикл программы
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        // Обработка событий
+        glfwPollEvents();
+
+        // Начало нового кадра ImGui
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();    
+        ImGui::NewFrame();
+
+        // Создание пользовательского интерфейса ImGui
+        ImGui::Begin("Hello, ImGui!");
+        ImGui::Text("Welcome to ImGui!");
+        if (ImGui::Button("Click Me!"))
+        {
+        }
+
+
+
+        ImGui::End();
+                // Рендеринг ImGui
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         
-    //     // Swap буферов
-    //     glfwSwapBuffers(window);
-    // }
+        // Swap буферов
+        glfwSwapBuffers(window);
+    }
     
-    // // Очистка и выход из программы
-    // glfwTerminate();
+    // Очистка и выход из программы
+    glfwTerminate();
 
     return 0;
 }
